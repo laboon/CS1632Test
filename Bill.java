1911b70fcee8f0efb7cdbcf8d5c7f139b7c7ea49
laboon <laboon@gmail.com>
2016-06-08 16:04:10 -0400

    Went back

--- Bill.java
+++ Bill.java
@@ -1,10 +1,8 @@
 public class Bill {
 
     public static void main(String[] args) {
-	int j = 4;
-	
 	while (true) {
-	    System.out.println("Bill drools!");
+	    System.out.println("Bill rules!");
 	}
 
     }
