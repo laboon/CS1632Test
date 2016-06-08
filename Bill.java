0445e7936ba89540082e3266221e8c66ad36422e
laboon <laboon@gmail.com>
2016-06-08 16:02:34 -0400

    Drools

--- Bill.java
+++ Bill.java
@@ -2,9 +2,9 @@ public class Bill {
 
     public static void main(String[] args) {
 	int j = 4;
-	int k = 7;
+	
 	while (true) {
-	    System.out.println("Bill rules!");
+	    System.out.println("Bill drools!");
 	}
 
     }
